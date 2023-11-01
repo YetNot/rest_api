567ded975f7317cbfb538fc917fd873290342beb
Kirill Glinskikh <glinskix24@bk.ru>
2023-11-01 18:17:42 +0300

    middleware

--- internal/apperror/middleware.go
+++ internal/apperror/middleware.go
@@ -1,9 +1,32 @@
 package apperror
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+)
 
-type appHandler func (w http.ResponseWriter, r *http.Request) error
+type appHandler func(w http.ResponseWriter, r *http.Request) error
 
 func Middleware(h appHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
+	return func(w http.ResponseWriter, r *http.Request) {
+		var appErr *AppError
+		if err := h(w, r); err != nil {
+			w.Header().Set("Content-Type", "application/type")
+			if errors.As(err, &appErr) {
+				if errors.Is(err, ErrNotFound) {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write(ErrNotFound.Marshal())
+					return
+				}
+
+				err = err.(*AppError)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write(ErrNotFound.Marshal())
+
+			}
+
+			w.WriteHeader(http.StatusTeapot)
+			w.Write(systemError(err).Marshal())
+		}
+	}
 }
